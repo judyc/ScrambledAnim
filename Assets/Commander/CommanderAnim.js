0321ba59061caa0dfd864881d2aa2bb8bf4e566b
Judy Chang <jchang.ty@gmail.com>
2014-02-24 15:43:25 -0500

    Commander Animation Base Scene commit. File clean up.

---
+++ Assets/Commander/CommanderAnim.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+var clip: AnimationClip[];
+var anim: Animator;
+//get current animation state
+var currentState: AnimatorStateInfo;
+
+function Start () {
+
+}
+
+function Update () {
+    //triggerAnim();
+}
+
+function FixedUpdate(){
+    anim = GetComponent(Animator);
+    currentState = anim.GetCurrentAnimatorStateInfo(0);
+
+    if(Input.GetButtonDown("beeAttack")){
+        anim.SetBool("beeAttack", true);
+    } else anim.SetBool("beeAttack", false);
+}
+
+function triggerAnim(){
+
+    /*-----Legacy Animation Trigger Script
+
+    if(Input.GetKey ("g")){
+        animation.Play(clip[1].name);
+    } else animation.Play(clip[0].name);
+
+    */
+}
+
